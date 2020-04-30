3973de4d92e0364f2c78cb859fa3ba40317cd74d
liyuhao <liyuhaoid@sina.com>
2020-04-30 14:26:36 +0800

    准备提交更新

---
+++ lib_utils/src/main/java/spa/lyh/cn/lib_utils/SPUtils.java
@@ -0,0 +1,68 @@
+package spa.lyh.cn.lib_utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SPUtils {
+    private static String FILLNAME = "config";// 文件名称
+    private static SharedPreferences mSharedPreferences = null;
+
+    /**
+     * 单例模式
+     */
+    public static synchronized SharedPreferences getInstance(Context context) {
+        if (mSharedPreferences == null) {
+            mSharedPreferences = context.getApplicationContext().getSharedPreferences(FILLNAME, Context.MODE_PRIVATE);
+        }
+        return mSharedPreferences;
+    }
+
+    /**
+     * SharedPreferences常用的10个操作方法
+     */
+    public static void putBoolean(String key, boolean value, Context context) {
+        SPUtils.getInstance(context).edit().putBoolean(key, value).apply();
+    }
+
+    public static boolean getBoolean(String key, boolean defValue, Context context) {
+        return SPUtils.getInstance(context).getBoolean(key, defValue);
+    }
+
+    public static void putString(String key, String value, Context context) {
+        SPUtils.getInstance(context).edit().putString(key, value).apply();
+    }
+
+    public static String getString(String key, String defValue, Context context) {
+        return SPUtils.getInstance(context).getString(key, defValue);
+    }
+
+    public static void putInt(String key, int value, Context context) {
+        SPUtils.getInstance(context).edit().putInt(key, value).apply();
+    }
+
+    public static int getInt(String key, int defValue, Context context) {
+        return SPUtils.getInstance(context).getInt(key, defValue);
+    }
+
+    public static void putLong(String key, long value, Context context) {
+        SPUtils.getInstance(context).edit().putLong(key, value).apply();
+    }
+
+    public static long getLong(String key, int defValue, Context context) {
+        return SPUtils.getInstance(context).getLong(key, defValue);
+    }
+
+    /**
+     * 移除某个key值已经对应的值
+     */
+    public static void remove(String key, Context context) {
+        SPUtils.getInstance(context).edit().remove(key).apply();
+    }
+
+    /**
+     * 清除所有内容
+     */
+    public static void clear(Context context) {
+        SPUtils.getInstance(context).edit().clear().apply();
+    }
+}
--- lib_utils/src/main/java/spa/lyh/cn/lib_utils/TimeUtils.java
+++ lib_utils/src/main/java/spa/lyh/cn/lib_utils/TimeUtils.java
@@ -1,6 +1,13 @@
 package spa.lyh.cn.lib_utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 public class TimeUtils {
+    private final static long second = 1000;
+    private final static long minute = 60 * 1000;// 1分钟
+    private final static long hour = 60 * minute;// 1小时
+    private final static long day = 24 * hour;// 1天
 
     /**
      * 毫秒转换分：秒
@@ -22,5 +29,50 @@ public class TimeUtils {
             diffTime=diffTime+":"+second;
         }
         return diffTime;
+    }
+
+    /**
+     * 时间戳转多少天前的格式
+     * @return
+     */
+    public static String getShowTime(long timeSeconds){
+//        long diff = getLongToString(getLosAngelesTime()) -timeSeconds;  //0;//当前时间 - 得到时间
+        long time = System.currentTimeMillis();
+        long diff = time - timeSeconds;  //0;//当前时间 - 得到时间
+        long r= 0;
+
+        if (diff > second){
+            r = (diff /second);
+            if (r < 59){//大于秒
+                return  r +"秒前";
+            }
+        }
+
+        if (diff > minute){
+            r = (diff / minute);
+            if (r < 59){
+                return  r +"分钟前";
+            }
+        }
+
+        if (diff > hour){
+            r = (diff / hour);
+            if (r < 24){
+                return  r + "小时前";
+            }
+        }
+
+        return getCurrentTimeToString(timeSeconds,"yyyy-MM-dd");
+    }
+
+    /**
+     * 将时间戳转换成String
+     * @param timeSeconds
+     * @return
+     */
+    public static String getCurrentTimeToString(long timeSeconds,String pattern){
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern,
+                Locale.getDefault());
+        return sdf.format(timeSeconds);
     }
 }
