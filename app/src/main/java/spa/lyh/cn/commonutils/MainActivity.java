e86cd473679284ecddfa6a08fd928b9a0df27023
liyuhao <liyuhaoid@sina.com>
2020-05-06 23:55:22 +0800

    继续更新内容，关于navigationbar

--- app/src/main/java/spa/lyh/cn/commonutils/MainActivity.java
+++ app/src/main/java/spa/lyh/cn/commonutils/MainActivity.java
@@ -1,11 +1,14 @@
 package spa.lyh.cn.commonutils;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import spa.lyh.cn.lib_utils.AppUtils;
@@ -15,15 +18,37 @@ import spa.lyh.cn.lib_utils.view.EmptyItemAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv;
+    View vv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.tv);
+        vv = findViewById(R.id.nav_bar);
         //Toast.makeText(this, AppUtils.checkDeviceHasNavigationBar(this)+"",Toast.LENGTH_SHORT).show();
         //Toast.makeText(this, AppUtils.isNetworkAvailable(this)+"",Toast.LENGTH_SHORT).show();
         //EmptyItemAnimator myItemAnimator = new EmptyItemAnimator();
-        //Log.e("qwer","高度："+PixelUtils.getNavigationBarHeight(this));
-        BarUtils.autoFitStatusBar(this,R.id.status_bar,new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,0));
-        //BarUtils.autoFitNavigationBar(this,R.id.nav_bar,new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,PixelUtils.getNavigationBarHeight(this)));
+        Log.e("qwer","高度："+PixelUtils.getNavigationBarHeight(this));
+        BarUtils.autoFitStatusBar(this,R.id.status_bar);
+        BarUtils.autoFitNavigationBar(this,R.id.nav_bar);
+        //AppUtils.setSystemUiVisibility(getWindow().getDecorView(), View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR,false);
+        /*new Handler(getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                AppUtils.setSystemUiVisibility(getWindow().getDecorView(), View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR,false);
+            }
+        },1000);*/
+
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("qwer","点击");
+                vv.setBackgroundColor(getResources().getColor(R.color.black));
+                //AppUtils.setSystemUiVisibility(getWindow().getDecorView(), View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR,true);
+            }
+        });
     }
 }
--- lib_utils/src/main/java/spa/lyh/cn/lib_utils/translucent/BarUtils.java
+++ lib_utils/src/main/java/spa/lyh/cn/lib_utils/translucent/BarUtils.java
@@ -4,17 +4,19 @@ import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 
+import spa.lyh.cn.lib_utils.PixelUtils;
+
 public class BarUtils {
 
-    public static void autoFitStatusBar(Activity activity, int barId, ViewGroup.LayoutParams layoutParams){
+    public static void autoFitStatusBar(Activity activity, int barId){
         View statusbar = activity.findViewById(barId);
-        statusbar.setLayoutParams(layoutParams);
+        statusbar.getLayoutParams().height = PixelUtils.getStatusBarHeight(activity);
         TranslucentUtils.setTranslucentTOP(activity);
     }
 
-    public static void autoFitNavigationBar(Activity activity, int barId, ViewGroup.LayoutParams layoutParams){
+    public static void autoFitNavigationBar(Activity activity, int barId){
         View navigationbar = activity.findViewById(barId);
-        navigationbar.setLayoutParams(layoutParams);
-        TranslucentUtils.setTranslucentBottom(activity);
+        navigationbar.getLayoutParams().height = PixelUtils.getNavigationBarHeight(activity);
+        TranslucentUtils.setTranslucentBoth(activity);
     }
 }
